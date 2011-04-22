da336e24c47b737bfbbe9312524236d9b33dc2e2
Tony Hannan <tony@10gen.com>
2011-04-22 16:48:47 -0400

    move registrar back to here

---
+++ registrar.cpp
@@ -0,0 +1,4 @@
+
+#include "registrar.h"
+
+std::map <std::string, std::map <long, boost::shared_ptr<void> > > registrar::Registry;
---
+++ registrar.h
@@ -0,0 +1,64 @@
+/* Local registry of objects referenced by id. Useful for remote reference to an object (see remote/ref.h) */
+
+#ifndef REGISTRAR_H_
+#define REGISTRAR_H_
+
+#include <string>
+#include <sstream>
+#include <map>
+#include <utility>
+#include <typeinfo>
+#include <boost/shared_ptr.hpp>
+
+namespace registrar {
+
+	/** Map type id and name to object */
+	extern std::map <std::string, std::map <long, boost::shared_ptr<void> > > Registry;
+
+	/** Reference to an object of type T on local host */
+	template <class T> class Ref {
+	public: // private to this file
+		uintptr_t id;
+		Ref (uintptr_t id) : id(id) {}
+		Ref () {}  // for serialization
+	// public
+		/** Fetch object from registry, null ptr if missing */
+		boost::shared_ptr<T> deref () {
+			return boost::static_pointer_cast <T> (Registry [typeid(T).name()] [id]);
+		}
+		T* operator-> () {
+			return deref().get();
+		}
+		T& operator* () {
+			return *deref();
+		}
+		void remove() {
+			Registry [typeid(T).name()] .erase (id);
+		}
+	};
+
+	/** Register T object using its address as its id. Ref<T>(id) refers to it */
+	template <class T> Ref<T> add (boost::shared_ptr<T> p) {
+		uintptr_t id = (uintptr_t) p.get();
+		Registry [typeid(T).name()] [id] = boost::static_pointer_cast <void, T> (p);
+		return Ref<T> (id);
+	}
+
+	template <class T> void remove (boost::shared_ptr<T> p) {
+		Registry [typeid(T).name()] .erase ((uintptr_t) p.get());
+	}
+
+}
+
+/* Serialization */
+
+namespace boost {
+namespace serialization {
+
+template <class Archive, class T> void serialize (Archive & ar, registrar::Ref<T> & x, const unsigned version) {
+	ar & x.id;
+}
+
+}}
+
+#endif /* REGISTRAR_H_ */
